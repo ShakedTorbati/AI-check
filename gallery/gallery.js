b4f066c6bef49e45a56ccef3e15bf83eda3cc235
ShakedTorbati <shakedtorb@gmail.com>
2024-04-07 19:27:44 +0300

    on

---
+++ gallery/gallery.js
@@ -0,0 +1,33 @@
+function barack() { 
+   var load = document.getElementById('loading');
+   setTimeout(clean, 1000);
+   window.location.href = "../check/check.html";
+}
+
+function clean(){
+   console.log("clean");
+}
+
+ function chatgpt(){
+    window.location.href="../check/check.html";
+ }
+ function dalle(){
+    window.location.href="../check/check.html";
+ }
+ function damsel(){
+    window.location.href="../check/check.html";
+ }
+ function nba(){
+    window.location.href="../check/check.html";
+ }
+ function neymar(){
+    window.location.href="../check/check.html";
+ }
+ function telescope(){
+    window.location.href="../check/check.html";
+ }
+ function vegatables(){
+    window.location.href="../check/check.html";
+ } function virus(){
+    window.location.href="../check/check.html";
+ }

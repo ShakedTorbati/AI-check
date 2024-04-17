6858a28b08aab4adf9a67e162448593cea90fe4e
shellyraz6 <shellyraz6@gmail.com>
2024-04-17 18:59:05 +0300

    header

---
+++ header.js
@@ -0,0 +1,122 @@
+let header = document.getElementById("header");
+header.innerHTML=`
+<div id="topall">
+    <div align="right" id="toppix"> 
+     <a href= "../html/home.html"><img src = "../img/logo2.png"/></a>
+    </div>
+
+    <div align="right" id="topbuttons">
+        <button class="buttons" id="homebutton" type="submit" onclick="gotohome()">home</button>
+         <button class="buttons" id="gallerybutton" type="submit" onclick="gotostore()">gallery</button>
+         <button class="buttons" id="aboutbutton" type="submit" onclick="gotoaboutus()">about us</button>
+    </div>
+</div>
+ 
+
+<style>
+#topbuttons{
+    display: flex;
+    flex-direction: row;
+    justify-content:end;
+
+    height: 60px;
+
+   
+    
+    
+}
+
+#toptext{
+    font-size:25px;
+    width:200px;
+    justify-content:space-between;
+
+
+}
+#toppix{
+    width:650px;
+    height:150px;
+}
+#topall{
+display: flex;
+flex-direction: row;
+justify-content:space-between;
+height: 150px;
+background-color:#A8DDE1;
+color:Teal;
+font-family:Oswald;
+
+}
+
+#homebutton{
+    width: 100px;
+    font-size: 20px;
+    text-align:center; 
+
+}
+
+#gallerybutton{
+    width: 100px;
+    font-size: 20px;
+    text-align:center; 
+
+}
+
+#aboutusbutton{
+    width: 100px;
+    font-size: 20px;
+    text-align:center;  
+
+}
+
+
+
+body {
+align-items: center;
+background-color:Lavender;
+
+ }
+
+img{
+height:50%;
+width: 50%;
+}
+
+#homebutton{
+    color:rgb(47, 91, 87);
+}
+
+.homebutton:hover, #homebutton:hover {
+    background-color:DarkSalmon;
+
+}
+#gallerybutton{
+    color:rgb(47, 91, 87);
+}
+
+.gallerybutton:hover, #gallerybutton:hover {
+    background-color:DarkSalmon;
+
+}
+
+#aboutbutton{
+    color:rgb(47, 91, 87);
+
+}
+
+.aboutbutton:hover, #aboutbutton:hover {
+    background-color:DarkSalmon;
+}
+</style>`
+
+function gotohome(){
+    window.location.href="../html/homepage.html";
+}
+
+function gotogallery(){
+    window.location.href="../html/gallery.html";
+}
+
+function gotoabout(){
+    window.location.href="../index.html";
+}

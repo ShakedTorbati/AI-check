6c8da304cdd931300e7db8dcabee0f0c0d42ee43
ShakedTorbati <shakedtorb@gmail.com>
2024-04-19 12:07:13 +0300

    Fvbhfb

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,94 +1,95 @@
 
 
-function barack() {    
-   var all= document.getElementById('everything');
-   var load= document.getElementById('loading');     
-   load.style.display="block"; 
-   all.style.display="none"; 
-   setTimeout(clean, 1000); 
-   console.log("clean2");    
-  
+function barack() {  
+  alert("hello")  
+  var all= document.getElementById('everything');
+  var load= document.getElementById('loading');     
+  load.style.display="block"; 
+  all.style.display="none"; 
+  setTimeout(clean, 1000); 
+  console.log("clean2");    
+ 
 }
 
 function clean(){
-   var all= document.getElementById('everything');
-   var load= document.getElementById('loading');  
-   load.style.display="none"; 
-   all.style.display="none";   
-   console.log("clean");
+  var all= document.getElementById('everything');
+  var load= document.getElementById('loading');  
+  load.style.display="none"; 
+  all.style.display="none";   
+  console.log("clean");
 
 }
 
- function chatgpt(){
-   var all= document.getElementById('everything');
-   var load= document.getElementById('loading');     
-   load.style.display="block"; 
-   all.style.display="none"; 
-   setTimeout(clean, 1000); 
-   console.log("clean2"); 
- }
- function dalle(){
-   var all= document.getElementById('everything');
-   var load= document.getElementById('loading');     
-   load.style.display="block"; 
-   all.style.display="none"; 
-   setTimeout(clean, 1000); 
-   console.log("clean2"); 
- }
- 
- function damsel(){
-   var all= document.getElementById('everything');
-   var load= document.getElementById('loading');     
-   load.style.display="block"; 
-   all.style.display="none"; 
-   setTimeout(clean, 1000); 
-   console.log("clean2"); 
- }
+function chatgpt(){
+  var all= document.getElementById('everything');
+  var load= document.getElementById('loading');     
+  load.style.display="block"; 
+  all.style.display="none"; 
+  setTimeout(clean, 1000); 
+  console.log("clean2"); 
+}
+function dalle(){
+  var all= document.getElementById('everything');
+  var load= document.getElementById('loading');     
+  load.style.display="block"; 
+  all.style.display="none"; 
+  setTimeout(clean, 1000); 
+  console.log("clean2"); 
+}
 
- function nba(){
-   var all= document.getElementById('everything');
-   var load= document.getElementById('loading');     
-   load.style.display="block"; 
-   all.style.display="none"; 
-   setTimeout(clean, 1000); 
-   console.log("clean2"); 
- }
+function damsel(){
+  var all= document.getElementById('everything');
+  var load= document.getElementById('loading');     
+  load.style.display="block"; 
+  all.style.display="none"; 
+  setTimeout(clean, 1000); 
+  console.log("clean2"); 
+}
 
- function neymar(){
-   var all= document.getElementById('everything');
-   var load= document.getElementById('loading');     
-   load.style.display="block"; 
-   all.style.display="none"; 
-   setTimeout(clean, 1000); 
-   console.log("clean2"); 
- }
+function nba(){
+  var all= document.getElementById('everything');
+  var load= document.getElementById('loading');     
+  load.style.display="block"; 
+  all.style.display="none"; 
+  setTimeout(clean, 1000); 
+  console.log("clean2"); 
+}
 
- function telescope(){
-   var all= document.getElementById('everything');
-   var load= document.getElementById('loading');     
-   load.style.display="block"; 
-   all.style.display="none"; 
-   setTimeout(clean, 1000); 
-   console.log("clean2"); 
- }
+function neymar(){
+  var all= document.getElementById('everything');
+  var load= document.getElementById('loading');     
+  load.style.display="block"; 
+  all.style.display="none"; 
+  setTimeout(clean, 1000); 
+  console.log("clean2"); 
+}
 
- function vegatables(){
-   var all= document.getElementById('everything');
-   var load= document.getElementById('loading');     
-   load.style.display="block"; 
-   all.style.display="none"; 
-   setTimeout(clean, 1000); 
-   console.log("clean2"); 
- } 
- 
- function virus(){
-   var all= document.getElementById('everything');
-   var load= document.getElementById('loading');     
-   load.style.display="block"; 
-   all.style.display="none"; 
-   setTimeout(clean, 1000); 
-   console.log("clean2"); 
- }
+function telescope(){
+  var all= document.getElementById('everything');
+  var load= document.getElementById('loading');     
+  load.style.display="block"; 
+  all.style.display="none"; 
+  setTimeout(clean, 1000); 
+  console.log("clean2"); 
+}
+
+function vegatables(){
+  var all= document.getElementById('everything');
+  var load= document.getElementById('loading');     
+  load.style.display="block"; 
+  all.style.display="none"; 
+  setTimeout(clean, 1000); 
+  console.log("clean2"); 
+} 
+
+function virus(){
+  var all= document.getElementById('everything');
+  var load= document.getElementById('loading');     
+  load.style.display="block"; 
+  all.style.display="none"; 
+  setTimeout(clean, 1000); 
+  console.log("clean2"); 
+}
 true=0;
 false=0;
  //This function simulate the check with google and ai,

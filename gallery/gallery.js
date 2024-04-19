b45cf02b96be9c214749c85c1b98107bb822d1d5
ShakedTorbati <shakedtorb@gmail.com>
2024-04-20 01:52:06 +0300

    gallery check

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -3,18 +3,36 @@ function barack() {
   var load = document.getElementById('loading');
   load.style.display = "block";
   all.style.display = "none";
-  setTimeout(clean, 1000);
+  setTimeout(clean_barack, 1000);
   console.log("clean2");
-  var data = document.getElementById('everything');
-}
 
+}
+function answers_barack() {
+  var date = document.getElementById('answers-date');
+  var header = document.getElementById('answers-header_check');
+  var google = document.getElementById('answers-google_check');
+  var ai = document.getElementById('answers-ai');
+  var final = document.getElementById('final');
+  date.innerText = "1.date:MARCH 18, 2024 6:35 PM CET-True"
+  header.innerText = "2.header check:the title versus the article - the same topic-True"
+  google.innerText = "3.google check:checking how many international news sites have the same case-True"
+  ai.innerText = "4.ai check-True"
+  final.innerText = "100% True, 0% False, THE ARTICALE IS TRUE"
+}
+function clean_barack() {
+  var all = document.getElementById('everything');
+  var load = document.getElementById('loading');
+  load.style.display = "none";
+  all.style.display = "none";
+  console.log("clean");
+  answers_barack()
+}
 function clean() {
   var all = document.getElementById('everything');
   var load = document.getElementById('loading');
   load.style.display = "none";
   all.style.display = "none";
   console.log("clean");
-
 }
 
 function chatgpt() {
@@ -22,8 +40,30 @@ function chatgpt() {
   var load = document.getElementById('loading');
   load.style.display = "block";
   all.style.display = "none";
-  setTimeout(clean, 1000);
+  setTimeout(clean_chatgpt, 1000);
   console.log("clean2");
+
+}
+function answers_chatgpt() {
+  var date = document.getElementById('answers-date');
+  var header = document.getElementById('answers-header_check');
+  var google = document.getElementById('answers-google_check');
+  var ai = document.getElementById('answers-ai');
+  var final = document.getElementById('final');
+  date.innerText = "1. Date: March 25, 2024 - True";
+  header.innerText = "2. Header check: The title versus the article - the same topic - True";
+  google.innerText = "3. Google check - False";
+  ai.innerText = "4. AI check - Not Sure";
+  final.innerText = "50% True, 50% False, THE ARTICLE IS FALSE";
+}
+
+function clean_chatgpt() {
+  var all = document.getElementById('everything');
+  var load = document.getElementById('loading');
+  load.style.display = "none";
+  all.style.display = "none";
+  console.log("clean");
+  answers_chatgpt();
 }
 function dalle() {
   var all = document.getElementById('everything');
